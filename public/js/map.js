7fd5be6e1b736fc45f5a862d349886189919ef3e
zhangqi3939 <37658979+zhangqi3939@users.noreply.github.com>
2020-03-30 14:56:44 +0800

    Revert "耗材统计增加rfid，删除前端内容"
    This reverts commit b8b912993393e7d4144f423cf2fa46d6d101c61d.

---
+++ public/js/map.js
@@ -0,0 +1,27 @@
+var map;
+var markClusterer;
+initMap();
+function initMap(){
+  map = null;
+  map = new BMap.Map('mapContent');
+  point = new BMap.Point(101.404,35.917);
+  map.centerAndZoom(point, 5);
+  map.setMinZoom(0);
+  map.enableScrollWheelZoom();
+  var top_left_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_LEFT}); 
+		map.addControl(top_left_navigation);
+		//清除当前地图内容
+		map.clearOverlays();
+		window.map = map;
+}
+function openPoint(row){
+  var lng = row.lng;
+  var lat = row.lat;
+  point = new BMap.Point(lng,lat);
+  map.centerAndZoom(point, 15);
+  map.clearOverlays();
+  var pt = new BMap.Point(lng, lat);
+  var myIcon = new BMap.Icon("images/addr.png", new BMap.Size(19,50));
+  var marker2 = new BMap.Marker(pt,{icon:myIcon});
+  map.addOverlay(marker2);          
+}
