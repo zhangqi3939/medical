0d7624d36152754470a6742f60029735b154c847
winsig <winsig@126.com>
2020-06-17 11:25:26 +0800

    增加sublime项目文件到ignor

--- application/behavior/Cors.php
+++ application/behavior/Cors.php
@@ -7,7 +7,7 @@ class Cors extends Controller
     public function appInit(&$params)
     {
         header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
+        header("Access-Control-Allow-Headers: Token,Origin, X-Requested-With, Content-Type, Accept");
         header('Access-Control-Allow-Methods: POST,GET,OPTIONS');
         if (request()->isOptions()) {
             exit();
